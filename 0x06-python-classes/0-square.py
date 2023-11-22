f515f2b1a0daacd3bf676d3bdc85009b28c4918f
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-11-22 01:42:39 +0100

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Created on Tue Nov 21 15:40:54 2023
+@author: Igbineweka Osaretin Monday
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+    pass
