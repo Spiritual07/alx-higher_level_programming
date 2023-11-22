a315bb83d73725157a6209294d11ac214129246c
Igbineweka <kelvin.igbineweka@yahoo.com>
2023-11-22 01:46:58 +0100

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Created on Tue Nov 21 15:59:54 2023
+@author: Igbineweka Osaretin Monday
+"""
+
+class Square:
+    """Class Square that has attributes. Instantiation with size
+
+    Attributes:
+        size (int): The size of the square
+    """
+
+    def __init__(self, size):
+        """The __init__ method for Square class
+
+        Args:
+            size: (:obj: 'int'): A private instance size
+        """
+        self.__size = size
