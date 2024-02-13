42c31e8b128e7fcbffd4fdafd2e2de2545cbf102
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-02-13 20:32:21 +0100

    Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- Import in hbtn_0c_0 database this table dump
--- Displays cities avg temperature
-SELECT city, AVG(value) AS avg_temp 
-FROM temperatures 
-WHERE value IS NOT NULL 
+-- Displays avg temp in July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month=7 OR month=8
 GROUP BY city 
-ORDER BY avg_temp DESC;
+ORDER BY avg_temp DESC LIMIT 3;
