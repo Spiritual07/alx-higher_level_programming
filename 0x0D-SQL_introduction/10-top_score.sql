27e989d8443a9e28d5fc153492d86bca0212585b
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-02-13 18:27:18 +0100

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table second_table
+-- Records should be ordered by score (top first)
+-- Results should display both the score and the name
+SELECT score, name FROM second_table ORDER BY score DESC, name;
