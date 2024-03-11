324f91448302085ce0f9a385f63d179f07ea235d
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-11 20:46:44 +0100

    Print three lines using array of strings and a loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let lineCounter;
+
+for (lineCounter = 0; lineCounter < 3; lineCounter++) {
+  console.log(lines[lineCounter]);
+}
