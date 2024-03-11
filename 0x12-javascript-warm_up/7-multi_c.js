21b3b8681d655650d70a81ef6298320ca66cd839
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-11 21:08:54 +0100

    A script that prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const convertedNumber = parseInt(process.argv[2]);
+let lineCounter;
+
+if (process.argv.length == 2 || isNaN(convertedNumber)) {
+ console.log('Missing number of occurrences');
+} else {
+  for (lineCounter = 0; lineCounter < convertedNumber; lineCounter++) {
+    console.log("C is fun");
+  }
+}
