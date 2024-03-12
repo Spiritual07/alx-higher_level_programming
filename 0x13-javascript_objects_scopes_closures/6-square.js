58856134c38bfadb596377198c586a338d1dfc19
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-12 17:21:47 +0100

    A class Square that defines a square and inherits from Square of 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const SquareK = require('./5-square.js');
+
+class Square extends SquareK {
+  charPrint (c) {
+    for (let x = 0; x < this.height; x++) {
+      let row = '';
+      for (let y = 0; y < this.width; y++) {
+        if (c === undefined) {
+          row += 'X';
+        } else {
+          row += 'c';
+        }
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
