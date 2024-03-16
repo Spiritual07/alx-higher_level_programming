024f4e717840076c0ddd70dd01bdc2a656ec04ce
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-16 19:35:41 +0100

    lists all State objects that contain the letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states(username, password, database_name):
+    engine = create_engine(f"""mysql+mysqldb://{username}:{password}
+                            @localhost:3306/{database_name}""")
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    list_states(username, password, database_name)
