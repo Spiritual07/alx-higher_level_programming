a18417a75b23b76a9d36fbf6169623a4f51dae7b
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-17 10:48:14 +0100

    Cities in state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-A  file that contains the class definition of a State
-and an instance Base = declarative_base()
-"""
 
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
