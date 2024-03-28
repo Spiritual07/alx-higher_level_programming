44763f0570a8cc9069732fd812b8b364276fedc0
Igbineweka <kelvin.igbineweka@yahoo.com>
2024-03-28 12:05:49 +0100

    python network 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of
+the response (decoded in utf-8)
+"""
+
+import sys
+import urllib.request
+import urllib.parse
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+if __name__ == "__main__":
+    parse_data = urllib.parse.urlencode({"email:", email})
+    data = parse_data.encode('utf-8')
+
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+    print(body)
